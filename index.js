1f4ce45193bdd5ee601b05f3a8e979036098f60e
celgutierrez <celgutierrez42@gmail.com>
2017-06-08 12:25:50 -0700

    added add'l animations, changed real-nav styling slightly

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
-var typewriter = require('typewriter-js');
+// var typewriter = require('typewriter-js');
 var app = express();
 
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/static'));
-app.use(typewriter);
+// app.use(typewriter);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
@@ -27,5 +27,6 @@ app.get('/projects', function(req, res) {
 app.get('/contact', function(req, res) {
     res.render('contact');
 });
+
 
 app.listen(process.env.PORT || 3000);
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
     $('.tooltipped').tooltip({ delay: 10 });
-    typewriter.prepare('.typewriter');
-    typewriter.type('.typewriter');
 
 });
