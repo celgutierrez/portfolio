abe36096d3b6c965cc6978248dea9302c6cf6889
celgutierrez <celgutierrez42@gmail.com>
2017-06-09 02:21:48 -0700

    fixed profile pic, finished about/contact/projects, created add'l navbar

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
     $('.tooltipped').tooltip({ delay: 10 });
+    $(".button-collapse").sideNav({
+        menuWidth: 100
+    });
+
+    $(function() {
+        $("#typing_text").typed({
+            strings: ["Hello World,", "this is amazing."],
+            typeSpeed: 0
+        });
+    });
 
 });
