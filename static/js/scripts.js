994d58de5f4cbd788220adcf8216cdc868929bc8
celina <celgutierrez42@gmail.com>
2017-08-08 12:00:24 -0700

    fixed menu ratio for mobile

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('.tooltipped').tooltip({ delay: 10 });
     $(".button-collapse").sideNav({
-        menuWidth: 100
+        menuWidth: 250
     });
     $('.carousel').carousel();
     $(".type").typed({
