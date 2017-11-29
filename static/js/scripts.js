5b2d923722af729a94fdd97b45b1a5ff942d7b12
celina <celgutierrez42@gmail.com>
2017-11-28 23:50:06 -0800

    updated fonts, realigned home, added more navigation on home

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.tooltipped').tooltip({ delay: 10 });
     $(".button-collapse").sideNav({
         menuWidth: 250
-    });
+    });   
     $('.carousel').carousel();
     $(".type").typed({
         strings: ['Artist', 'Activist', 'Full Stack Web Developer'],
@@ -10,6 +10,5 @@ $(document).ready(function() {
         typeSpeed: 100,
         loop: false
     });
-
 
 });
